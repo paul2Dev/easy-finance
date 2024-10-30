8a91e4aa69fd4e60362ff871ee0a4bcfbe805af8
Paul2D <paul.maxineanu@gmail.com>
2024-10-30 11:38:25 +0200

    configure user roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -44,5 +46,17 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessFilament(): bool
+    {
+        // Only allow users with the 'Admin' role to access Filament
+        return $this->hasRole('Admin');
+    }
+
+    public function canAccessPanel($panel): bool
+    {
+        // Apply similar logic here or customize it further if needed
+        return $this->canAccessFilament();
     }
 }
