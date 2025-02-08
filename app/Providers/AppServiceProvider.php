16ae7f339c20478e7389139e8a904d4854790b4d
Paul2D <paul.maxineanu@gmail.com>
2025-02-08 22:49:32 +0200

    comment log infos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,14 +26,14 @@ class AppServiceProvider extends ServiceProvider
     {
 
         EditAction::configureUsing(function (EditAction $action) {
-            Log::info('EditAction global configuration applied.');
+            //Log::info('EditAction global configuration applied.');
             $action->slideOver();
         });
 
         //i want the same thing for the CreateAction with the corrent CreateAction namespace
 
         CreateAction::configureUsing(function (CreateAction $action) {
-            Log::info('CreateAction global configuration applied.');
+            //Log::info('CreateAction global configuration applied.');
             $action->slideOver();
         });
 
